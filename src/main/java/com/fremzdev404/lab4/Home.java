0f4345f197f2750de2473f7072677afa15230764
fremznginx <nutthabodee_jon@vu.ac.th>
2018-12-13 16:48:20 +0700

    Add OK Button

--- src/main/java/com/fremzdev404/lab4/Home.java
+++ src/main/java/com/fremzdev404/lab4/Home.java
@@ -1,5 +1,6 @@
 package com.fremzdev404.lab4;
 
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -14,16 +15,16 @@ import android.widget.Toast;
 import java.util.Random;
 
 public class Home extends AppCompatActivity {
-    TextView tvCS,tvTeamFootball,tvProfile;
-    EditText etName,etHeight,etWeight,etPhone;
-    RadioButton rtMale,rtFemale;
+    TextView tvCS, tvTeamFootball, tvProfile;
+    EditText etName, etHeight, etWeight, etPhone;
+    RadioButton rtMale, rtFemale;
     ImageView imTeamFootball;
 
 
     String[] colorCode = {"#FF6633", "#FFB399'", "#FF33FF", "#FFFF99", "#00B3E6",
             "#E6B333", "#3366E6", "#999966", "#99FF99", "#B34D4D",};
 
-    String[] CLUBS = {"FC Barcelona","Real Madrid","Liverpool","PSG","Dortmund","Beyern Munich"};
+    String[] CLUBS = {"FC Barcelona", "Real Madrid", "Liverpool", "PSG", "Dortmund", "Beyern Munich"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +38,7 @@ public class Home extends AppCompatActivity {
         etPhone = findViewById(R.id.etPhone);
         rtMale = findViewById(R.id.radioMale);
         rtFemale = findViewById(R.id.radioFemale);
+        tvProfile = findViewById(R.id.tvProfile);
 
         tvCS.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,28 +50,100 @@ public class Home extends AppCompatActivity {
             }
         });
     }
+
     public void clickButtonTv1(View v) {
         tvCS.setTextSize(20);
         Toast.makeText(this, tvCS.getText(),
-        Toast.LENGTH_LONG).show();
+                Toast.LENGTH_LONG).show();
     }
-    public void showAlertDialogPopup(View v){
+
+    public void showAlertDialogPopup(View v) {
         boolean male = rtMale.isChecked();
         boolean female = rtFemale.isChecked();
         String sex = "";
-        if(male == true)
+        if (male == true)
             sex = "ชาย";
-        if(female == true)
+        if (female == true)
             sex = "หญิง";
 
         AlertDialog.Builder adb = new AlertDialog.Builder(this);
         AlertDialog ad = adb.create();
         ad.setTitle("ประวัติส่วนตัว");
         ad.setMessage("ชื่อ : " + etName.getText() + "\n" +
-        "ส่วนสูง : " + etHeight.getText() + "\n" +
-        "น้ำหนัก : " + etWeight.getText() + "\n" +
-        "เพศ : " + sex + "\n" +
-        "โทรศัพท์ : " + etPhone.getText());
+                "ส่วนสูง : " + etHeight.getText() + "\n" +
+                "น้ำหนัก : " + etWeight.getText() + "\n" +
+                "เพศ : " + sex + "\n" +
+                "โทรศัพท์ : " + etPhone.getText());
         ad.show();
+    }
+
+    public void showAlertDialogPopup2(View v) {
+        boolean male = rtMale.isChecked();
+        boolean female = rtFemale.isChecked();
+        String sex = "";
+        if (male == true)
+            sex = "ชาย";
+        if (female == true)
+            sex = "หญิง";
+
+        AlertDialog.Builder adb = new AlertDialog.Builder(this);
+        adb.setTitle("ประวัติส่วนตัว");
+        adb.setIcon(R.drawable.person1);
+        adb.setMessage("ชื่อ : " + etName.getText() + "\n" +
+                "ส่วนสูง : " + etHeight.getText() + "\n" +
+                "น้ำหนัก : " + etWeight.getText() + "\n" +
+                "เพศ : " + sex + "\n" +
+                "โทรศัพท์ : " + etPhone.getText());
+        adb.setPositiveButton("Close", null);
+        adb.show();
+    }
+    public void showAlertDialogPopup3(View v) {
+        boolean male = rtMale.isChecked();
+        boolean female = rtFemale.isChecked();
+        String sex = "";
+        if (male == true)
+            sex = "ชาย";
+        if (female == true)
+            sex = "หญิง";
+
+        AlertDialog.Builder adb = new AlertDialog.Builder(this);
+        adb.setTitle("ประวัติส่วนตัว");
+        adb.setIcon(R.drawable.person1);
+        adb.setMessage("ชื่อ : " + etName.getText() + "\n" +
+                "ส่วนสูง : " + etHeight.getText() + "\n" +
+                "น้ำหนัก : " + etWeight.getText() + "\n" +
+                "เพศ : " + sex + "\n" +
+                "โทรศัพท์ : " + etPhone.getText());
+        adb.setPositiveButton("Clear", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                etName.setText(null);
+                etHeight.setText(null);
+                etWeight.setText(null);
+                etPhone.setText(null);
+                rtMale.setChecked(false);
+                rtFemale.setChecked(false);
+            }
+        });
+        adb.setNegativeButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                boolean male = rtMale.isChecked();
+                boolean female = rtFemale.isChecked();
+                String sex = "";
+                if (male == true)
+                    sex = "ชาย";
+                if (female == true)
+                    sex = "หญิง";
+                tvProfile.setText("ชื่อ : " + etName.getText() + "\n" +
+                        "ส่วนสูง : " + etHeight.getText() + "\n" +
+                        "น้ำหนัก : " + etWeight.getText() + "\n" +
+                        "เพศ : " + sex + "\n" +
+                        "โทรศัพท์ : " + etPhone.getText());
+                tvProfile.setTextSize(20);
+                tvProfile.setTextColor(Color.MAGENTA);
+            }
+        });
+        adb.show();
     }
 }
